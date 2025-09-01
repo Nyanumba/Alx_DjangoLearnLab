60c0f1846982629ec95dc1253ca41311807ef646
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2025-09-01 20:08:36 +0300

    created login, register and logiut urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 def list_books(request):
     books = Book.objects.all()
@@ -11,3 +13,14 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+    
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto login after registration
+            return redirect("list_books")  # redirect to book list or homepage
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
