e65ee96c23441d91283d3e9ee3454a3af5d4112f
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2025-09-08 15:59:36 +0300

    fixed urls mismatch with previous instructions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,21 +1,36 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
-
+from .views import (
+    list_books,
+    LibraryDetailView,
+    register,
+    admin_view,
+    librarian_view,
+    member_view,
+    add_book,
+    edit_book,
+    delete_book,
+)
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),  # FBV
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # CBV
-    # authentiction
+    # Function-based view
+    path("books/", list_books, name="list_books"),
+
+    # Class-based view
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+
+    # Authentication
     path("login/", auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", views.register, name="register"),
+    path("register/", register, name="register"),
+
     # Role-based access control
-    path("admin-view/", views.admin_view, name="admin_view"),
-    path("librarian-view/", views.librarian_view, name="librarian_view"),
-    path("member-view/", views.member_view, name="member_view"),
-    # permissions
-     path("add_book/", views.add_book, name="add_book"),
-    path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
-    path("delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
+    path("admin-view/", admin_view, name="admin_view"),
+    path("librarian-view/", librarian_view, name="librarian_view"),
+    path("member-view/", member_view, name="member_view"),
+
+    # Permissions
+    path("add_book/", add_book, name="add_book"),
+    path("edit_book/<int:book_id>/", edit_book, name="edit_book"),
+    path("delete_book/<int:book_id>/", delete_book, name="delete_book"),
 ]
