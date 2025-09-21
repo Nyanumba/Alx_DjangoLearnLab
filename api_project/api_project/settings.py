ad0f315ea81d262c4c82f68372eb43239be3bd3b
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2025-09-21 21:12:03 +0300

    created the book model in the api app and installed a rest_framework

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1^mh6l(e0k4scvd1=)8873k-m!+ks7_9av#u4)c^_)jhjs3ttx'
+SECRET_KEY = 'django-insecure-rv#lobdv+ix02^wdd&1tnli%zfoi$(kyjy86$n!8a+w9eds-aw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
