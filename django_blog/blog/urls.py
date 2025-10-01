5e4e06a86a2938605bc7f2fb49b2d2152fa9b16d
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2025-10-01 13:36:36 +0300

    corrected the spelling error

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
     
     #urls for comments
-    path("posts/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment_create"),
+    path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment_create"),
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment_update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment_delete"),
 ]
